a33b93948fe5750d44eb149d5abec14d2cc1952b
Anuj Kumar Singh <anujk01997@gmail.com>
2023-05-14 14:07:39 +0530

    commit_nxtPatch

--- src/modules/login/loginService.js
+++ src/modules/login/loginService.js
@@ -16,32 +16,40 @@ exports.getLogin = async (req, res) => {
     let isCollectExist = common.isCollectionExist(collectionList, header.collection);
     if (isCollectExist) {
         let result = await dbResource.collection(header.collection).find({}).toArray();
-        result.forEach(async (x) => {
-            if (x.userName == body.userName && x.password == body.password && x.login_status == false) {
-                let generatedToken = jwt.sign({ _id: x._id }, 'saral_lms');
+        for(let i= 0; i < result.length; i++){
+            console.log("ESPA ", result[0]);
+            if (result[i].userName == body.userName && result[i].password == body.password && (result[i].login_status == false || body?.userResponse)) {
+                let generatedToken = jwt.sign({ _id: result[i]._id }, 'saral_lms');
                 let jwtDecode = jwt.verify(generatedToken, 'saral_lms');
-                response.data.token = generatedToken;
-                response.data[0] = { ...x },
+                response.data[0] = { ...result[i] },
                 delete response.data[0].password;
+                delete response.data[0].token;
+                delete response.data[0].token_expiry;
+                console.log("AQS:", response.data[0]);
+                response.data[0].token = generatedToken;
+                response.data[0].token_expiry = jwtDecode.iat;
                 response.success = true,
                 response.status_code = 200,
                 response.message = "Login Success",
-                dbResource.collection(header.collection).updateOne({ userName: x.userName }, { $set: { token: generatedToken, token_expiry: jwtDecode.iat, login_status: true, last_login: Date(new Date()) } });
-            } else if (x.userName == body.userName && x.password == body.password && x.login_status == true  && !body?.userResponse) {
+                console.log("SLA", response.data[0]);
+                dbResource.collection(header.collection).updateOne({ userName: result[i].userName }, { $set: { token: generatedToken, token_expiry: jwtDecode.iat, login_status: true, last_login: Date(new Date()) } });
+                
+            } else if (result[i].userName == body.userName && result[i].password == body.password && result[i].login_status == true  && !body?.userResponse) {
                     response.success = true,
                     response.status_code = 401,
                     response.message = "User Already have Active Session. Are you sure to Inactive Earlier Session ??"
-            } else if (x.userName == body.userName && body?.userResponse == true) {
-                let generatedToken = jwt.sign({ _id: x._id }, 'saral_lms');
-                response.data.token = generatedToken;
-                response.data[0] = { ...x },
-                delete response.data[0].password;
-                response.success = true,
-                response.status_code = 200,
-                response.message = "Login Success",
-                dbResource.collection(header.collection).updateOne({ userName: x.userName }, { $set: { token: generatedToken, login_status: true, last_login: Date(new Date()) } });
-            }
-        })
+            } 
+            // else if (i.userName == body.userName && body?.userResponse == true) {
+            //     let generatedToken = jwt.sign({ _id: i._id }, 'saral_lms');
+            //     response.data.token = generatedToken;
+            //     response.data[0] = { ...i },
+            //     delete response.data[0].password;
+            //     response.success = true,
+            //     response.status_code = 200,
+            //     response.message = "Login Success",
+            //     dbResource.collection(header.collection).updateOne({ userName: i.userName }, { $set: { token: generatedToken, login_status: true, last_login: Date(new Date()) } });
+            // }
+        }
     } else {
         response.message = "Collection not exist !",
         response.status_code = 101
@@ -65,7 +73,9 @@ exports.isLogged = async (req, res) => {
         let result = await dbResource.collection(header.collection).find({}).toArray();
         if (body.access_token) {
             let jwtDecode = jwt.verify(body.access_token, 'saral_lms');
+            console.log(">>>>", jwtDecode.iat);
             result.forEach(async (x) => {
+                console.log("<<<<", x.token_expiry);
                 if (x._id == jwtDecode._id && x.token_expiry == jwtDecode.iat ) {
                     const token_created_date = new Date(x.token_created);
                     if ((Date.parse(x.last_password_change) < Date.parse(token_created_date))) {
@@ -74,15 +84,15 @@ exports.isLogged = async (req, res) => {
                         response.success = true,
                         response.status_code = 200,
                         response.message = "Login Success"
-                    } else {
+                    } else if((Date.parse(x.last_password_change) > Date.parse(token_created_date))) {
                         response.success = true,
                         response.status_code = 403,
                         response.message = "Password Changed"
-                    }
-                } else {
+                    } else {
                     response.success = true,
                     response.status_code = 502,
                     response.message = "Session Expired"
+                }
                 }
             });
         } else {
